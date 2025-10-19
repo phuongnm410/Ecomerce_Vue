905cfc3fe0d3612415463283b11a6889c16f9621
unknown <nmp24141@gmail.com>
2025-10-19 15:46:21 +0700

    add Header

---
+++ src/components/Header.vue
@@ -0,0 +1,95 @@
+<template>
+    <header class="sticky top-0 z-50 transition-all duration-300">
+        <section :class="['w-full', isScrolled ? 'bg-gray-100/95 backdrop-blur shadow-md py-2' : 'bg-gray-100 py-4']">
+            <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+                <div class="flex flex-col md:flex-row items-center justify-between gap md:gap-6">
+                    <!-- Logo + Mobile Menu Toggle   -->
+                    <div class="flex justify-between items-center w-full md:w-auto">
+                        <a href="" class="texxt-2xl font-bold text-pink-600">Shop Ease</a>
+                        <button class="md:hidden text-gray-700 hover:text-indigo-600" arria-label="Toggle Menu"
+                            @click="isMobileMenuOpen = !isMobileMenuOpen">
+                            <Icon icon="mdi:menu" class="w-6 h-6"></Icon>
+                        </button>
+                    </div>
+
+                    <!-- Search Bar -->
+                    <form class="w-full md:flex-1 max-ww-sm" role="search" aria-label="Site search">
+                        <label class="relative w-full" for="">
+                            <input type="text" placeholder="Search....."
+                                class="w-full px-3 py-3 text-xs border border-gray-300 rounded-full focus:outline-none focus:ring-2 focus:ring-indigo-500">
+                        </label>
+                        <button type="submit"
+                            class="absolute right-2 top-1/2 -translate-y-1/2 text-gray-500 hover:text-indio-600"
+                            aria-label="Search button">
+                            <Icon icon="mdi:magnify" class="w-4 h-4"></Icon>
+                        </button>
+
+
+                    </form>
+
+                    <!-- Icon -->
+                    <aside class="flex items-center justify-end space-x-4 w-full md:w-auto ">
+                        <button class="relavtive p-2 text-gray-700 hover:text-pink-600" aria-label="Wishlist">
+                            <Icon icon="mdi:heart-outline" class="w-5 h-5"></Icon>
+                            <span
+                                class="absolute -top-1 -right-1 bg-pink-500 text-white text-xs rounded-full h-5 w-5 flex items-center justify-center">3</span>
+                        </button>
+                        <button class="relavtive p-2 text-gray-700 hover:text-pink-600" aria-label="Cart">
+                            <Icon icon="mdi:cart-outline" class="w-5 h-5"></Icon>
+                            <span
+                                class="absolute -top-1 -right-1 bg-pink-500 text-white text-xs rounded-full h-5 w-5 flex items-center justify-center">5</span>
+                        </button>
+                        <button class="relavtive p-2 text-gray-700 hover:text-pink-600" aria-label="Account">
+                            <Icon icon="account:cart-outline" class="w-5 h-5"></Icon>
+                        </button>
+                    </aside>
+                </div>
+            </div>
+        </section>
+
+        <!-- Navbar   -->
+
+        <nav class="bg-pink-950" aria-label="Main navigation">
+            <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+                <ul class="hidden md:flex justify-center py-3 flex-wrap gap-x-6 text-sm font-medium text-white">
+                    <li v-for="item in navItems" :key="item.id">
+                        <a href="item.link" class="hover:text-pink-300 transition-colors"> {{ item.name }}</a>
+                    </li>
+                </ul>
+                <section v-if="isMobileMenuOpen" class="md:hidden mt-2 bg-white rounded-lg shadow-md p-4 space-y-3 text-[#5D4037] text-center">
+                    <a v-for="item in navItems" :href="item.link" :key="item.id" class="block hover:text-amber-600 text-sm font-medium">
+                        {{ item.name }}
+                    </a>
+                </section>
+            </div>
+
+        </nav>
+
+
+    </header>
+</template>
+<script setup>
+import { ref,onMounted, onUnmounted} from 'vue';
+const isScrolled = ref(false);
+const isMobileMenuOpen = ref(false);
+const navItems = [
+    { id: 1, nam: 'Home', link: '#home' },
+    { id: 2, nam: 'Products', link: '#products' },
+    { id: 3, nam: 'Categories', link: '#categories' },
+    { id: 4, nam: 'Offers', link: '#offers' },
+    { id: 5, nam: 'About', link: '#about' },
+    { id: 6, nam: 'Contact', link: '#contact' },
+];
+
+const handleScroll = () => {
+    isScrolled.value = window.scrollY > 10;
+};
+onMounted(() => {
+   window.addEventListener('scroll', handleScroll);
+});
+onUnmounted(() => {
+    window.removeEventListener('scroll', handleScroll);
+});
+
+
+</script>
---
+++ src/main.js
@@ -0,0 +1,5 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+import { Icon } from '@iconify/vue'
+createApp(App).component("Icon", Icon).mount('#app')
